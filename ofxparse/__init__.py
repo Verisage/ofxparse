7b72fd5f9f77a5231c647da8193194c823552773
Guy Harding <utahguy@users.noreply.github.com>
2023-06-14 20:00:15 -0600

    fix typo

--- ofxparse/__init__.py
+++ ofxparse/__init__.py
@@ -4,7 +4,7 @@ from .ofxparse import (OfxParser, OfxParserException, AccountType, Account,
                        Statement, Transaction)
 from .ofxprinter import OfxPrinter
 
-__version__ = '0.21.1
+__version__ = '0.21.1'
 __all__ = [
     'OfxParser',
     'OfxParserException',
